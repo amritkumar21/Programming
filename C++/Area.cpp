027ba9b218d73017b5551410112b2472b6442097
Amrit Kumar <amrit.973@live.com>
2022-08-14 15:55:01 +0530

    Added C++ programs

---
+++ C++/Area.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int area(int l, int b)
+{
+  return l*b;
+}
+float area(int r)
+{
+  return 3.14*r*r;
+}
+
+int main()
+{
+  int r;
+  cout<<"area of circle is"<<area(6);
+  cout<<"area of rectangle is"<<area(3,4);
+
+}
